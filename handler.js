a02cfb48bd1fab9e8d642c4a769ccdbca3d29288
Isabella Firmino <isabella.firmino@watchlabs.media>
2025-04-03 14:04:10 -0300

    updating files for aws lambda test

--- handler.js
+++ handler.js
@@ -1,4 +1,10 @@
 import serverlessExpress from "@vendia/serverless-express";
 import { app } from "./app.js";
 
-export const api = serverlessExpress({ app });
+export const api = async (event, context) => {
+  if (!event.requestContext) {
+    event.requestContext = context; 
+  }
+
+  return serverlessExpress({ app })(event, context);
+};
