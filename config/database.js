df38f2ee47712d20d3fa55f02dd12aebe6d838b6
Isabella Firmino <isabella.firmino@watchlabs.media>
2025-04-03 00:34:35 -0300

    configuring remote database

--- config/database.js
+++ config/database.js
@@ -1,27 +1,14 @@
 const { Sequelize } = require('sequelize');
-require('dotenv').config();
 
-const sequelize = new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PASSWORD,
-    {
-        host: process.env.DB_HOST,
-        port: process.env.DB_PORT,
-        dialect: process.env.DB_DIALECT,
-        logging: false, 
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  dialect: 'postgres',
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false
     }
-);
-
-(async () => {
-    try {
-        await sequelize.authenticate();
-        console.log('Conexão com o PostgreSQL estabelecida com sucesso.');
-    } catch (error) {
-        console.error('Erro ao conectar no PostgreSQL:', error);
-    }
-})();
+  },
+  logging: false, 
+});
 
 module.exports = sequelize;
-
-
