3a560d23f47bd03a156aaecf865603cdf87c4194
Isabella Firmino <isabella.firmino@watchlabs.media>
2025-04-03 02:01:37 -0300

    updating config

--- config/database.js
+++ config/database.js
@@ -1,14 +1,18 @@
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize(process.env.DATABASE_URL, {
-  dialect: 'postgres',
-  dialectOptions: {
-    ssl: {
-      require: true,
-      rejectUnauthorized: false
-    }
-  },
-  logging: false, 
-});
+const sequelize = new Sequelize({
+    database: "neondb",
+    username: "neondb_owner",
+    password: "npg_Nmy3iEBsg9wl",
+    host: "ep-curly-glitter-a4k4bhiq-pooler.us-east-1.aws.neon.tech",
+    dialect: "postgres",
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
+    logging: false,
+  });
 
 module.exports = sequelize;
