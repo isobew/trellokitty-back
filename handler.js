4d2130042ccd0a51cd5ecad41cbce8fb95874687
Isabella Firmino <isabella.firmino@watchlabs.media>
2025-04-03 03:01:43 -0300

    updating

--- handler.js
+++ handler.js
@@ -1,14 +1,4 @@
 const serverless = require("serverless-http");
-const app = require("./app");
+const { app } = require("./app");
 
-module.exports.api = serverless(app, {
-    request: function (req, event) {
-        if (event.body) {
-            try {
-                req.body = JSON.parse(event.body); // 🚀 Força a conversão do JSON corretamente
-            } catch (err) {
-                console.error("Erro ao converter JSON:", err);
-            }
-        }
-    }
-});
+module.exports.api = serverless(app);
