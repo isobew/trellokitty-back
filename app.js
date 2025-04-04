d1d0f3b50ad81f05c76772ec6c3c8a9abb477d5f
Isabella Firmino <isabella.firmino@watchlabs.media>
2025-04-03 22:15:43 -0300

    trying to fix cors

--- app.js
+++ app.js
@@ -2,8 +2,10 @@ import "dotenv/config";
 import express from "express";
 import routes from "./src/routes.js";
 import sequelize from "./config/database.js";
+import cors from "cors";
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 
